b92fbbb13ac18985c9ab1c95c74cb74c3699483a
bartek8654 <40451206+bartek8654@users.noreply.github.com>
2019-06-08 15:16:28 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
 
 double *wynik;
 
-void checkValid(char line[])
-{
+void checkValid(char line[]){
     int numbers = 0,operators = 0;
     int length = strlen(line) - 1;
 
@@ -16,12 +16,17 @@ void checkValid(char line[])
         {
             operators++;
         }
-        if( isdigit( line[i] ) && (line[i+1]==' ' || line[i+1]=='\n'))
+        if( isdigit( line[i] ) && (line[i+1]==' ' || line[i+1]=='\n' ))
         {
             numbers++;
         }
-    }
 
+        if(isalpha(line[i]))
+        {
+            printf("Incorect input data!\n");
+            exit(0);
+        }
+    }
     if((operators<1) || numbers-1!=operators)
     {
         printf("Incorect input data!\n");
@@ -29,167 +34,208 @@ void checkValid(char line[])
     }
 }
 
-void calculate(char *operations,int *position,char line[],int result)
-{
-    double suma = 0;
 
-    for(int i=0;i<strlen(operations);i++)
-    {
-        if(!(isdigit(line[position[i]-2])) || !(isdigit(line[position[i]+2])) )
+
+void calculate(char line[],int result){
+
+    int i;
+    for(i = 0;i<=strlen(line);i++){
+        double leftValue = 0;
+        double rightValue= 0;
+
+        bool leftFloat = false;
+        bool rightFloat = false;
+
+        double suma = 0;
+        int k = 0;
+        char l[10] = " ";
+        char r[10] = " ";
+
+        int lB = 0;
+        int rB = 0;
+
+        if((line[i]=='/' || line[i]=='*' || line[i]=='+' || line[i]=='-') && (line[i-1]==' ') && (line[i+1]==' ') )
         {
-            if(line[position[i]+2]!='-')
+            if(!(isdigit(line[i-2])) || !(isdigit(line[i+2])) ){
+                if(line[i+2]!='-')
+                {
+                    printf("Incorect input data\n");
+                    exit(0);
+                }
+            }
+            bool isProrityD = false;
+            bool isProrityM = false;
+
+            if(line[i]!='/')
+            {
+                for(int ii = i;ii<=strlen(line);ii++){
+                    if(line[ii]=='/') isProrityD = true;
+                }
+
+                for(int ii = i;ii<=strlen(line);ii++){
+                    if(line[ii]=='*') isProrityM = true;
+                }
+            }
+            else
+            {
+                isProrityD = true;
+            }
+
+            if((isProrityD && line[i]=='/') || ( isProrityD==false && isProrityM && line[i]=='*') || (isProrityD==false && isProrityM==false) )
+            {
+                 while(line[i-2-k]!=' ' && i-2-k >=0)
+                {
+                    if(line[i-2-k]!='.' && line[i-2-k]!='-' && !(isdigit(line[i-2-k])) )
+                    {
+                        printf("Incorect input data!\n");
+                        exit(0);
+                    }
+                    l[k] = line[i-2-k];
+
+                    if(line[i-2-k]=='.')
+                    {
+                        leftFloat = true;
+                    }
+                    k++;
+
+                    if(line[i-2-k]=='-')
+                    {
+
+                        l[k] = '-';
+                        k++;
+                        break;
+                    }
+                }
+
+                lB = i-2-k;
+                l[k] = '\0';
+
+                if(strlen(l)>1)
+                {
+                    char g[10];
+                    int u;
+                    for(u=0;u<strlen(l);u++)
+                    {
+                        g[u] = l[strlen(l)-1-u];
+                    }
+
+                    g[u] = '\0';
+
+                    if(g[u-1]=='.')
+                    {
+                        printf("Incorect input data!\n");
+                        exit(0);
+                    }
+
+                    if(leftFloat) leftValue = atof(&g);
+                    else leftValue = atoi(&g);
+            }
+            else
+            {
+                if(l[k-1]=='.')
+                {
+                    printf("Incorect input data!\n");
+                    exit(0);
+                }
+                if(leftFloat) leftValue = atof(&l);
+                else leftValue = atoi(&l);
+            }
+
+            k = 0;
+            int dd = 0;
+            while(line[i+2+k]!=' ' && line[i+2+k]!='\n')
+            {
+                if(line[i+2+k]!='.' && line[i+2+k]!='-' && !(isdigit(line[i+2+k])) )
+                {
+                    printf("Incorect input data!\n");
+                    exit(0);
+                }
+
+                r[k] = line[i+2+k];
+
+                if(line[i+2+k]=='.')
+                {
+                    rightFloat = true;
+                    dd++;
+                }
+                k++;
+            }
+
+            if(dd>1)
             {
                 printf("Incorect input data!\n");
                 exit(0);
             }
-        }
 
-        int jj;
-        int kk;
+            rB = i+2+k;
+            r[k] = '\0';
 
-        if(line[position[i]-3]!=' ' && isdigit(line[position[i]-3]))
-        {
-            char c[10] = " ";
-            char c2[10] = " ";
-            int k = 0;
-            while((line[position[i]-2-k]!=' ') && isdigit(line[position[i]-2-k]))
+
+            if(r[k-1]=='.')
             {
-                c[k] = line[position[i]-2-k];
-                k++;
-            }
-            if(line[position[i]-2-k]=='-')
-            {
-                c[k] = '-';
-                k++;
+                printf("Incorect input data!\n");
+                exit(0);
             }
 
-            c[k] = '\0';
+            if(rightFloat) rightValue = atof(&r);
+            else rightValue = atoi(&r);
 
-            int j = 0;
-            for(int i=strlen(c)-1;i>=0;i--)
-            {
-                c2[j] = c[i];
-                j++;
-            }
-            c2[j] = '\0';
+            if(line[i]=='*') suma = leftValue * rightValue;
+            else if(line[i]=='/') suma = leftValue / rightValue;
+            else if(line[i]=='+') suma = leftValue + rightValue;
+            else if(line[i]=='-') suma = leftValue - rightValue;
 
-            jj = atoi(&c2);
-            kk = atoi(&line[position[i]+2]);
-        }
-        else if(line[position[i]+3]!=' ' && isdigit(line[position[i]+3]))
-        {
-            char c[10] = " ";
-            int k = 0;
-            while((line[position[i]+2+k]!=' ') && (isdigit(line[position[i]+2+k]) || line[position[i]+2+k]=='-') )
-            {
-                c[k] = line[position[i]+2+k];
-                k++;
-            }
-            c[k] = '\0';
-            jj = atoi(&line[position[i]-2]);
-            kk = atoi(&c);
-        }
-        else
-        {
-            jj = atoi(&line[position[i]-2]);
-            kk = atoi(&line[position[i]+2]);
-        }
 
-        double j = (double) jj;
-        double k = (double) kk;
+            if(rightValue==0 && line[i]=='/')
+            {
+                printf("We don't divide by zero :) !\n");
+                exit(0);
+            }
 
-        if('+'==operations[i])
-        {
-            if(suma==0) suma = j + k;
-            else
+            char sum[20];
+            sprintf(sum, "%.2f", suma);
+            int d;
+            for(d=0;d<strlen(sum);d++);
+            sum[d] = '\0';
+
+            char tabx[50];
+            int x;
+            for(x=0;x<=lB;x++)
             {
-                if(position[i-1]!=NULL && position[i-1]>position[i])
-                {
-                    suma += j;
-                }
-                else if(position[i-1]!=NULL && position[i-1]<position[i])
-                {
-                    suma += k;
-                }
+                tabx[x] = line[x];
             }
+            tabx[x] = '\0';
+            strcat( tabx, &sum );
+
+            char taby[50];
+            int ii = 0;
+            int y;
+            for(y=rB;y<strlen(line);y++,ii++)
+            {
+                taby[ii] = line[y];
+            }
+
+            taby[ii] = '\0';
+            strcat( tabx, &taby );
+
+            int l;
+            for(l=0;l<strlen(tabx);l++)
+            {
+                line[l] = tabx[l];
+            }
+            line[l] = '\0';
+            i = 0;
+
+            isProrityD = false;
         }
-        else if('-'==operations[i])
-        {
-            if(suma==0)  suma = j - k;
-            else
-            {
-                if(position[i-1]!=NULL && position[i-1]>position[i])
-                {
-                    suma -= j;
-                }
-                else if(position[i-1]!=NULL && position[i-1]<position[i])
-                {
-                    suma -= k;
-                }
-            }
-        }
-        else if('*'==operations[i])
-        {
-            if(suma==0) suma = j * k;
-            else
-            {
-                if(position[i-1]!=NULL && position[i-1]>position[i])
-                {
-                    suma *= j;
-                }
-                else if(position[i-1]!=NULL && position[i-1]<position[i])
-                {
-                    suma *= k;
-                }
-            }
-        }
-        else if('/'==operations[i])
-        {
-            if(suma==0) suma = j / k;
-            else
-            {
-                if(position[i-1]!=NULL && position[i-1]>position[i])
-                {
-                    suma /= j;
-                }
-                else if(position[i-1]!=NULL && position[i-1]<position[i])
-                {
-                    suma /= k;
-                }
-            }
         }
     }
-
-    wynik[result] = suma;
+    wynik[result] = atof(line);
 }
 
-void orderOperation(char *operations,int *position){
-    int j = 0;
-    for(int i=0;i<strlen(operations);i++)
-    {
-        if(('/'==operations[i]) || ('*'==operations[i]) && i!=0)
-        {
-            int temp = position[i];
-            position[i] = position[j];
-            position[j] = temp;
-
-            char temp2 = operations[i];
-            operations[i] = operations[j];
-            operations[j] = temp2;
-            j++;
-        }
-    }
-}
-
-void explore(int *positions_of_oper, char *operations,int result)
+void explore(int result)
 {
-    char line[50]=" ";
-    char operation[10];
-    positions_of_oper = (int*) malloc (sizeof(int)*10);
-    for(int i=0;i<10;i++)
-    {
-        positions_of_oper[i] = 0;
-    }
+    char line[50] = " ";
 
     static int i = 0;
     fgets(line, sizeof line, stdin);
@@ -197,42 +243,14 @@ void explore(int *positions_of_oper, char *operations,int result)
     if(i!=0)
     {
         checkValid(line);
-
-        int j = 0;
-        for(int i=0;i<strlen(line);i++){
-
-
-            if(('+'==line[i]) || ('-'==line[i] && !(isdigit(line[i+1]))) ||  ('*'==line[i]) || ('/'==line[i]))
-            {
-                operation[j] = line[i];
-                positions_of_oper[j] = i;
-                j++;
-            }
-        }
-        operation[j] = '\0';
-
-        operations = (char*) malloc ((strlen(operation))*sizeof(char));
-
-        int k;
-        for(k=0;k<strlen(operation);k++)
-        {
-            operations[k] = operation[k];
-        }
-        operations[k] = '\0';
-
-        orderOperation(operations,positions_of_oper);
-
-        calculate(operations,positions_of_oper,line,result);
-
-        free(positions_of_oper);
-        free(operations);
+        calculate(line,result-1);
     }
     else i++;
 }
 
 void showResults(int line)
 {
-    for(int i=1;i<=line;i++)
+    for(int i=0;i<line;i++)
     {
         wynik[i] = floor(wynik[i]*100)/100;
         int truncated = (int)wynik[i];

42e0cb8692ed21b7039a826f9c4beb81b0cc32a5
bartek8654 <40451206+bartek8654@users.noreply.github.com>
2019-06-08 15:15:53 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,53 +1,43 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-
 /*@author   Bartosz Widera
-* @brief    Checking a correct number of number and operations
-* @param1    Line: Load line from console
+* @brief    Checks the correctness of the loaded line
+* @param1   line: Loaded line from console
 * @ret      void
 */
-void checkValid(char);
+void checkValid(char[]);
+
 
 
 /*@author   Bartosz Widera
-* @brief    Calculates the sum based on the sorted order of actions
-* @param1   operation: Array with sort operations
-* @param2   position: Array with sort positions of operations
-* @param3   line: Load line from console
-* @param4   result: Number of line
+* @brief    Calculates the sum based on the loaded line
+* @param1   Line: Loaded line from console
+* @param2   result: Number of line
 * @ret      void
 */
-void calculate(char *,int *,char,int);
+void calculate(char [],int);
+
 
 
 /*@author   Bartosz Widera
-* @brief    Sorting tables ( Priority mathematical operations)
-* @param1   operation: Allocated array ready for sorting( operations)
-* @param2   position: Allocated array ready for sorting ( positions of operations)
+* @brief    Gets loaded lines
+* @param1   result: Number of line
 * @ret      void
 */
-void orderOperation(char *,int *);
+void explore(int);
 
 
 
 /*@author   Bartosz Widera
-* @brief    gets loaded lines and reads individual operations
-* @param1   operation: Unallocated array with sort operations
-* @param2   position_of_oper: Unallocated array with sort positions of operations
-* @param3   result: Number of line
-* @ret      void
-*/
-void explore(int *, char *,int);
-
-
-
-/*@author   Bartosz Widera
-* @brief    Displays the result
-* @param1   line: Number of line
+* @brief    Displays the results
+* @param1   line: Number of lines
 * @ret      void
 */
 void showResults(int);
 
+#endif // MAIN_H
